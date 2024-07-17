33164e93c2a6e704105400779b1189fbcb247cb6
salman-eng1 <eng.salman2net@gmail.com>
2024-07-18 00:06:36 +0300

    setup apache config

--- src/controllers/deploy.ts
+++ src/controllers/deploy.ts
@@ -1,3 +1,4 @@
+import { createFile } from "@portal/services/create-file";
 import { execute } from "@portal/services/non-streamed-command"
 import { migrate, migrateFresh, systemProjects } from "@portal/services/sharedHelper";
 import { logger } from "@portal/utils/logging";
@@ -12,99 +13,126 @@ import { StatusCodes } from "http-status-codes";
 // }
 
 export const getSystems = async (_req: Request, res: Response): Promise<void> => {
-      try {
-        const projects = await execute("ls -l /var/www | grep '^d' | awk '{print $9}'", '');
-        const projectsArray = projects.split('\n').filter(project => project.trim() !== '');
-        res.status(StatusCodes.OK).json({ message: projectsArray });
-      } catch (err) {
-        logger.log('error', `Failed to retrieve projects`)
+  try {
+    const projects = await execute("ls -l /var/www | grep '^d' | awk '{print $9}'", '');
+    const projectsArray = projects.split('\n').filter(project => project.trim() !== '');
+    res.status(StatusCodes.OK).json({ message: projectsArray });
+  } catch (err) {
+    logger.log('error', `Failed to retrieve projects`)
 
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Failed to retrieve projects' });
-      }
-    };
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Failed to retrieve projects' });
+  }
+};
 
-    export const getSystemProjects = async (req: Request, res: Response): Promise<void> => {
-      try {
-            const systemName=req.body.systemName
-        const projectsArray :string []=await systemProjects(systemName)
-        res.status(StatusCodes.OK).json({ message: projectsArray });
-      } catch (err) {
-        logger.log('error', `Failed to retrieve sub projects`)
+export const getSystemProjects = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const systemName = req.body.systemName
+    const projectsArray: string[] = await systemProjects(systemName)
+    res.status(StatusCodes.OK).json({ message: projectsArray });
+  } catch (err) {
+    logger.log('error', `Failed to retrieve sub projects`)
 
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Failed to retrieve sub projects' });
-      }
-    };
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Failed to retrieve sub projects' });
+  }
+};
 
 
 
-    export const getEnabledProjects = async (_req: Request, res: Response): Promise<void> => {
-      try {
-        const projects = await execute("ls -l /etc/apache2/sites-enabled | awk '{print $9}'", '');
-        const enabledProjectsArray = projects.split('\n').filter(project => project.trim() !== '');
-        res.status(StatusCodes.OK).json({ message: enabledProjectsArray });
-      } catch (err) {
-        logger.log('error', `Failed to retrieve enabled projects`)
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Failed to retrieve enabled projects' });
-      }
-    };
+export const getEnabledProjects = async (_req: Request, res: Response): Promise<void> => {
+  try {
+    const projects = await execute("ls -l /etc/apache2/sites-enabled | awk '{print $9}'", '');
+    const enabledProjectsArray = projects.split('\n').filter(project => project.trim() !== '');
+    res.status(StatusCodes.OK).json({ message: enabledProjectsArray });
+  } catch (err) {
+    logger.log('error', `Failed to retrieve enabled projects`)
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Failed to retrieve enabled projects' });
+  }
+};
 
-    export const setupProject = async (req: Request, res: Response): Promise<void> => {
-      const systemName: string = req.body.systemName;
-      const projects: {
-        projectName: string,
-        domain: string,
-        port: number,
-        update: boolean,
-        composerUpdate: boolean,
-        composerInstall: boolean,
-        migrate: boolean,
-        migrateFresh: boolean,
-        gitlabUrl: string,
-        branchName: string
-      }[] = req.body.projects;
-    
-      let unavailableProjects: string[] = [];
-    
-      try {
-        const availableProjects = await systemProjects(systemName);
-    
-        for (const element of projects) {
-          if (!availableProjects.includes(element.projectName)) {
-            unavailableProjects.push(element.projectName);
+export const setupProject = async (req: Request, res: Response): Promise<void> => {
+  const systemName: string = req.body.systemName;
+  const projects: {
+    projectName: string,
+    domain: string,
+    port: number,
+    update: boolean,
+    composerUpdate: boolean,
+    composerInstall: boolean,
+    migrate: boolean,
+    migrateFresh: boolean,
+    gitlabUrl: string,
+    branchName: string
+  }[] = req.body.projects;
+
+  let unavailableProjects: string[] = [];
+
+  try {
+
+    await execute(`sed -i '/^Listen/d' /etc/apache2/ports.conf`, 'terminal');
+    await execute(`a2dissite /etc/apache2/sites-available/*`, 'terminal');
+    await execute(`rm /etc/apache2/sites-available/*`, 'terminal');
+
+    const availableProjects = await systemProjects(systemName);
+    let configContent = ``;
+    let filePath = ``;
+
+    for (const element of projects) {
+      if (!availableProjects.includes(element.projectName)) {
+        unavailableProjects.push(element.projectName);
+      } else {
+        if (element.update) {
+          // Update project
+          if (element.branchName) {
+            await execute(`cd /var/www/${systemName}/${element.projectName} && git checkout ${element.branchName} && git pull ${element.gitlabUrl} ${element.branchName}`, 'terminal');
           } else {
-            if (element.update) {
-              // Update project
-              await execute(`cd /var/www/${systemName}/${element.projectName} && git checkout ${element.branchName} && git pull ${element.gitlabUrl} ${element.branchName}`, 'terminal');
-            }
-            if (element.migrateFresh) {
-              // Fresh migrate project
-              await migrateFresh(systemName, element.projectName);
-            }
-            if (element.migrate) {
-              // Migrate project
-              await migrate(systemName, element.projectName);
-            }
-            if (element.composerInstall) {
-              // Install composer
-              await execute(`rm -r /var/www/${systemName}/${element.projectName}/vendor`, 'terminal');
-              await execute(`cd /var/www/${systemName}/${element.projectName} && composer install`, 'terminal');
-            }
-            if (element.composerUpdate) {
-              // Update composer
-              await execute(`cd /var/www/${systemName}/${element.projectName} && composer update`, 'terminal');
-            }
+            await execute(`cd /var/www/${systemName}/${element.projectName} && git pull ${element.gitlabUrl}`, 'terminal');
           }
         }
-    
-        if (unavailableProjects.length === 0) {
-          res.status(StatusCodes.OK).json({ message: 'Setup successful. All projects are available.' });
-        } else {
-          res.status(StatusCodes.OK).json({ message: `Please add projects [${unavailableProjects}] to /var/www` });
+        if (element.composerInstall) {
+          // Install composer
+          await execute(`rm -r /var/www/${systemName}/${element.projectName}/vendor`, 'terminal');
+          await execute(`cd /var/www/${systemName}/${element.projectName} && composer install`, 'terminal');
         }
-      } catch (err) {
-        logger.log('error', 'Error in setupProject()', `${err}`);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error processing request.' });
+        if (element.composerUpdate) {
+          // Update composer
+          await execute(`cd /var/www/${systemName}/${element.projectName} && composer update`, 'terminal');
+        }
+        if (element.migrateFresh) {
+          // Fresh migrate project
+          await migrateFresh(systemName, element.projectName);
+        }
+        if (element.migrate) {
+          // Migrate project
+          await migrate(systemName, element.projectName);
+        }
+        await execute(`echo Listen ${element.port}  >> /etc/apache2/ports.conf`, 'terminal');
+configContent = `
+    <VirtualHost *:${element.port}>
+        DocumentRoot /var/www/${systemName}/${element.projectName}/public
+        <Directory /var/www/${systemName}/${element.projectName}>
+            AllowOverride All
+            Order allow,deny
+            allow from all
+        </Directory>
+        ErrorLog \${APACHE_LOG_DIR}/${element.projectName}_error.log
+        CustomLog \${APACHE_LOG_DIR}/${element.projectName}_access.log combined
+    </VirtualHost>
+    `;
+filePath=`/etc/apache2/sites-available/${element.projectName}.conf`
+createFile(configContent,filePath)
       }
-    };
+    }
 
-    
+    if (unavailableProjects.length === 0) {
+      await execute(`a2ensite /etc/apache2/sites-available/*`, 'terminal');
+
+      res.status(StatusCodes.OK).json({ message: 'Setup successful. All projects are available.' });
+    } else {
+      res.status(StatusCodes.OK).json({ message: `Please add projects [${unavailableProjects}] to /var/www` });
+    }
+  } catch (err) {
+    logger.log('error', 'Error in setupProject()', `${err}`);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error processing request.' });
+  }
+};
+
--- src/services/network-settings.ts
+++ src/services/network-settings.ts
@@ -1,14 +1,13 @@
 import { readFile, writeFile } from 'fs';
 import { promisify } from 'util';
 import YAML from 'yaml';
-import { exec } from 'child_process';
+import { execute } from './non-streamed-command';
 
 
 
 // Promisify the readFile, writeFile, and exec functions
 const readFilePromise = promisify(readFile);
 const writeFilePromise = promisify(writeFile);
-const execPromise = promisify(exec);
 
 // Define the NetplanConfig type based on the structure of your netplan YAML configuration
 interface NetplanConfig {
@@ -36,7 +35,7 @@ export const updateNetplanIP = async (newIP: string, newMask: string,dns:string[
     const netplanConfig = await readFilePromise(netplanFilePath, 'utf8');
     const netplanData: NetplanConfig = YAML.parse(netplanConfig);
 
-    const currentIP = netplanData.network.ethernets.eth0.addresses ? netplanData.network.ethernets.eth0.addresses[0] : 'Not Assigned';
+    const currentIP = execute("'ip addr show eth0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1' ", 'terminal')
     // Backup the existing netplan configuration
     await writeFilePromise(netplanBackupFilePath, netplanConfig);
 
@@ -51,7 +50,10 @@ export const updateNetplanIP = async (newIP: string, newMask: string,dns:string[
     // Write the modified configuration back to the file
     const updatedNetplanConfig = YAML.stringify(netplanData);
     await writeFilePromise(netplanFilePath, updatedNetplanConfig);
-    await execPromise(`bash /home/zeuor/scripts/changeEnvIP.sh ${currentIP} ${newIP}`)
+    await execute(`bash /home/zeuor/scripts/changeEnvIP.sh ${currentIP} ${newIP} /var/www`, 'terminal')
+    await execute(`bash /home/zeuor/scripts/changeEnvIP.sh ${currentIP} ${newIP} /home/zeuor`,'terminal')
+
+    console.log(`${currentIP} ${newIP}`)
     // Apply the changes
     // await execPromise('netplan apply');
  
