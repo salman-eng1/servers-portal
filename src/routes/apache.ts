b5df078ef2ca22b3672c35c26882d26553bf4948
salman-eng1 <eng.salman2net@gmail.com>
2024-08-26 23:51:02 +0300

    test get sub system

--- src/routes/apache.ts
+++ src/routes/apache.ts
@@ -1,4 +1,4 @@
-import { disableSys, enableSys, getEnabledProjects } from '@portal/controllers/apache';
+import { enableSys, getEnabledProjects } from '@portal/controllers/apache';
 import { getSystemPorts } from '@portal/controllers/ports';
 
 
@@ -13,7 +13,7 @@ this.router=express.Router();
     public routes(){
      this.router.get('/get-system-ports',getSystemPorts)
         this.router.post('/enable-system',enableSys)
-        this.router.post('/disable-system',disableSys)
+        // this.router.post('/disable-system',disableSys)
         this.router.get('/get-enabled-projects',getEnabledProjects)
 
 
