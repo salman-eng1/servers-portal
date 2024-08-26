5f985b4c854e35c0ae796dd4322adc87508f5a3d
salman-eng1 <eng.salman2net@gmail.com>
2024-08-27 00:06:53 +0300

    test get sub system

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { troubleshootRoutes } from '@portal/routes/troubleshoot';
 import { deployRoutes } from '@portal/routes/enable-system';
 import { networkRoutes } from '@portal/routes/network-settings';
 import { apache } from '@portal/routes/apache';
+import { ports } from '@portal/routes/ports';
 
 
 export const appRoutes = (app: Application) => {
@@ -12,7 +13,7 @@ export const appRoutes = (app: Application) => {
   app.use('/api', troubleshootRoutes.routes());
   app.use('/api', deployRoutes.routes());
   app.use('/api', networkRoutes.routes());
-  // app.use('/api', ports.routes());
+  app.use('/api', ports.routes());
   app.use('/api', apache.routes());
 
 
