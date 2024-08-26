20246cfedb5fa49f0a85901507348c8ecf67ad8c
salman-eng1 <eng.salman2net@gmail.com>
2024-08-26 21:49:10 +0300

    test get sub system

--- src/controllers/network-settings.ts
+++ src/controllers/network-settings.ts
@@ -5,7 +5,7 @@ import { StatusCodes } from "http-status-codes";
 
 export const changeNetworkSettings = async (req: Request, res: Response): Promise<void> => {
   try {
-    updateNetplanIP(req.body.ip, req.body.mask, req.body.dns, req.body.gateway)
+    updateNetplanIP(req.body.ip, req.body.mask, req.body.dns)
     res.status(StatusCodes.OK).json({message: "server IP has been set successfully"})
   } catch (error) {
     logger.log('error','error in updateNetplanIp() method',`Failed to update netplan configuration: ${error}`);
--- src/services/network-settings.ts
+++ src/services/network-settings.ts
@@ -16,7 +16,6 @@ interface NetplanConfig {
       eth0: {
         dhcp4: boolean;
         addresses: string[];
-        gateway: string;
         nameservers: {
           addresses: string[]; // Dynamic array of nameserver addresses
         };
@@ -27,7 +26,7 @@ interface NetplanConfig {
 }
 
 
-export const updateNetplanIP = async (newIP: string, newMask: string,dns:string[],gateway:string): Promise<void> => {
+export const updateNetplanIP = async (newIP: string, newMask: string,dns:string[]): Promise<void> => {
     const netplanFilePath = '/etc/netplan/00-installer-config.yaml'; // Adjust if necessary
     const netplanBackupFilePath = '/etc/netplan/00-installer-config.yaml.bak';
 
@@ -44,7 +43,6 @@ export const updateNetplanIP = async (newIP: string, newMask: string,dns:string[
     netplanData.network.ethernets.eth0.dhcp4 = false;
     netplanData.network.ethernets.eth0.addresses = [`${newIP}/${newMask}`];
     netplanData.network.ethernets.eth0.nameservers.addresses = dns;
-    netplanData.network.ethernets.eth0.gateway = `${gateway}`;
 
 
 
