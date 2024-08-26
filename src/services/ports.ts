3b57a464b1cdce2c44ff8868b804df49ecabb83b
salman-eng1 <eng.salman2net@gmail.com>
2024-08-26 21:35:52 +0300

    test get sub system

--- src/services/ports.ts
+++ src/services/ports.ts
@@ -2,10 +2,13 @@ import { execute } from "@portal/services/non-streamed-command";
 
 export const getPorts = async (systemName: string): Promise<string[]> => {
   const systemPath = `/var/www/${systemName}`;
+  console.log('porrrrrrts')
   const envFilesString: string = await execute(
     `find ${systemPath} -type f -name '.env'`, 
     ''
   );
+  console.log('porrrrrrts')
+
   const envFiles: string[] = envFilesString.split('\n').filter(Boolean); // Filter out empty strings
   const ports: string[] = await Promise.all(
     envFiles.map(async (envFile) => {
@@ -13,6 +16,8 @@ export const getPorts = async (systemName: string): Promise<string[]> => {
         `grep -E '^APP_URL=' ${envFile} | awk -F '=' '{print $2}' | sed -n 's/.*:\\([0-9]\\+\\).*/\\1/p'`, 
         ''
       );
+      console.log('porrrrrrts')
+
       return port.trim() || '80'; // Trim newline and replace empty string with '80'
     })
   );
