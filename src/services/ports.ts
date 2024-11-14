5abbb4daf8c5b18617dcd0e7c1913bf44d982fcf
salman-eng1 <eng.salman2net@gmail.com>
2024-11-14 17:47:43 +0300

    setup server ready

--- src/services/ports.ts
+++ src/services/ports.ts
@@ -41,7 +41,7 @@ export const getPorts = async (systemName: string): Promise<string[]> => {
   
     const addedPorts: string[] = await Promise.all(
       ports.map(async (port) => {
-        const addCommand = `echo 'Listen ${port}' >> /etc/apache2/ports.conf && sed -i '/^Listen .*[^0-9]$/d' /etc/apache2/ports.conf`;
+        const addCommand = `echo 'Listen ${port}' >> /etc/apache2/ports.conf && sed -i '/^Listen[^0-9]*$/d' /etc/apache2/ports.conf        `;
         await execute(addCommand, '');
           return port;
       })
