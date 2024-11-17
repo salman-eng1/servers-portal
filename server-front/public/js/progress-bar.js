cc6de477c17092923094728c94ddb765cc8e23a4
salman-eng1 <eng.salman2net@gmail.com>
2024-11-17 17:56:01 +0300

    setup server ready

--- server-front/public/js/progress-bar.js
+++ server-front/public/js/progress-bar.js
@@ -1,22 +1,14 @@
-// Function to show the progress bar
-function showProgressBar() {
-    const progressBarContainer = document.getElementById('progress-bar-container');
-    progressBarContainer.style.display = 'block';
 
-    // Simulate progress bar movement
-    let width = 0;
-    const interval = setInterval(() => {
-        if (width >= 100) {
-            clearInterval(interval);
-        } else {
-            width += 5; // Adjust the step for the speed of progress
-            document.getElementById('progress-bar').style.width = width + '%';
-        }
-    }, 100); // Adjust interval time to control the speed of the progress
+function showProgressModal() {
+    const modal = document.getElementById('progress-modal');
+    if (modal) {
+        modal.style.display = 'flex'; // Show the modal
+    }
 }
 
-// Function to hide the progress bar
-function hideProgressBar() {
-    const progressBarContainer = document.getElementById('progress-bar-container');
-    progressBarContainer.style.display = 'none';
+function hideProgressModal() {
+    const modal = document.getElementById('progress-modal');
+    if (modal) {
+        modal.style.display = 'none'; // Hide the modal
+    }
 }
--- server-front/public/js/project.js
+++ server-front/public/js/project.js
@@ -99,16 +99,3 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-function showProgressModal() {
-    const modal = document.getElementById('progress-modal');
-    if (modal) {
-        modal.style.display = 'flex'; // Show the modal
-    }
-}
-
-function hideProgressModal() {
-    const modal = document.getElementById('progress-modal');
-    if (modal) {
-        modal.style.display = 'none'; // Hide the modal
-    }
-}
