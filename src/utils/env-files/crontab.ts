34bc594c115fc6938469ef3cd42b921463b37bbc
salman-eng1 <eng.salman2net@gmail.com>
2024-11-28 19:58:38 +0300

    setup serverA

--- src/utils/env-files/crontab.ts
+++ src/utils/env-files/crontab.ts
@@ -13,28 +13,19 @@ export const crontab = async (systemName: string): Promise<string> => {
     
 }else if(systemName === 'DGSN'){
     return `
-    */5 * * * *    root bash /home/logging_system/logging_script.sh
-    10 00 * * *    root    bash /home/zeuor/scripts/exportDB.sh
-    59 23 * * *    root    bash /home/zeuor/scripts/rotate_logs.sh
+    #######################${systemName}#######################
     `;
 }else if(systemName === 'VMS'){
     return `
-    */5 * * * *    root bash /home/logging_system/logging_script.sh
-    10 00 * * *    root    bash /home/zeuor/scripts/exportDB.sh
-    59 23 * * *    root    bash /home/zeuor/scripts/rotate_logs.sh
     */3 * * * *    root bash /home/zeuor/VMS/vms-reset.sh
     `;
 }else if(systemName === 'RMS'){
     return `
-    */5 * * * *    root bash /home/logging_system/logging_script.sh
-    10 00 * * *    root    bash /home/zeuor/scripts/exportDB.sh
-    59 23 * * *    root    bash /home/zeuor/scripts/rotate_logs.sh
+    #######################${systemName}#######################
     `;
 }else if(systemName === 'GLARUS_CONFIG'){
     return `
-    */5 * * * *    root bash /home/logging_system/logging_script.sh
-    10 00 * * *    root    bash /home/zeuor/scripts/exportDB.sh
-    59 23 * * *    root    bash /home/zeuor/scripts/rotate_logs.sh
+    #######################${systemName}#######################
     `;
 }else{
     throw Error ('error in creating crontab')
@@ -52,4 +43,7 @@ export const crontabCreate = async (): Promise<string> => {
     25 6    * * *   root    test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.daily )
     47 6    * * 7   root    test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.weekly )
     52 6    1 * *   root    test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.monthly )
+    */5 * * * *    root bash /home/logging_system/logging_script.sh
+    10 00 * * *    root    bash /home/zeuor/scripts/exportDB.sh
+    59 23 * * *    root    bash /home/zeuor/scripts/rotate_logs.sh
     `;}
