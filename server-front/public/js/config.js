fceb5054a7e56c2a118e4ffbd220378839d50fa7
Salman Ali <salman-eng1>
2024-12-05 02:34:19 +0300

    fixes

--- server-front/public/js/config.js
+++ server-front/public/js/config.js
@@ -1,14 +1,8 @@
-// window.APP_URL = 'http://192.168.1.27:5000';
-// window.Terminal_URL = 'http://192.168.1.27:4200';
-// window.phpmyadmin = 'http://192.168.1.27/phpmyadmin';
-// window.prometheus = 'http://192.168.1.27:4000';
+window.APP_URL = 'http://192.168.1.27:5000';
+window.Terminal_URL = 'http://192.168.1.27:4200';
+window.phpmyadmin = 'http://192.168.1.27/phpmyadmin';
+window.prometheus = 'http://192.168.1.27:4000';
 
-window.APP_URL_FRONT = 'http://127.0.0.1:5001';
-
-window.APP_URL = 'http://127.0.0.1:5001';
-window.Terminal_URL = 'http://127.0.0.1:4201';
-window.phpmyadmin = 'http://127.0.0.1/phpmyadmin';
-window.prometheus = 'http://127.0.0.1:4001';
 
 //  const EMAIL = "support@zeour.co.uk";
 //  const PASSWORD = "1234";
