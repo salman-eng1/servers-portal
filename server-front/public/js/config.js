5b71e86111695367a488cbb286bb6295c837a907
salman-eng1 <eng.salman2net@gmail.com>
2024-12-04 16:57:23 +0300

    setup serverA

--- server-front/public/js/config.js
+++ server-front/public/js/config.js
@@ -1,8 +1,8 @@
 // Attach the variable to the global window object
 //window.APP_URL = 'http://127.0.0.1:5000'; // Replace with the value from your .env if needed
 window.APP_URL = 'http://192.168.1.27:5000';
-window.Terminal_URL = 'http://192.168.1.27:6656'; // Replace with the value from your .env if needed
-//window.Terminal_URL = 'http://127.0.0.1:4200'; // Replace with the value from your .env if needed
+//window.Terminal_URL = 'http://192.168.1.27:4200'; // Replace with the value from your .env if needed
+window.Terminal_URL = 'http://192.168.1.27:4200'; // Replace with the value from your .env if needed
 
 // ./js/config.js
 //  const EMAIL = "support@zeour.co.uk";
