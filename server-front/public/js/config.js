cadd8493d48b151d58789c93d3fe3f37ba878463
salman-eng1 <eng.salman2net@gmail.com>
2024-12-05 01:35:58 +0300

    test

--- server-front/public/js/config.js
+++ server-front/public/js/config.js
@@ -1,13 +1,13 @@
-window.APP_URL = 'http://192.168.1.27:5000';
-window.Terminal_URL = 'http://192.168.1.27:4200';
-window.phpmyadmin = 'http://192.168.1.27/phpmyadmin';
-window.prometheus = 'http://192.168.1.27:4000';
+// window.APP_URL = 'http://192.168.1.27:5000';
+// window.Terminal_URL = 'http://192.168.1.27:4200';
+// window.phpmyadmin = 'http://192.168.1.27/phpmyadmin';
+// window.prometheus = 'http://192.168.1.27:4000';
 
 
-// window.APP_URL = 'http://127.0.0.1:5000';
-// window.Terminal_URL = 'http://127.0.0.1:4200';
-// window.phpmyadmin = 'http://127.0.0.1/phpmyadmin';
-// window.prometheus = 'http://127.0.0.1:4000';
+window.APP_URL = 'http://127.0.0.1:5000';
+window.Terminal_URL = 'http://127.0.0.1:4200';
+window.phpmyadmin = 'http://127.0.0.1/phpmyadmin';
+window.prometheus = 'http://127.0.0.1:4000';
 
 //  const EMAIL = "support@zeour.co.uk";
 //  const PASSWORD = "1234";
